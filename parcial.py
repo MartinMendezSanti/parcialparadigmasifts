7b8cedf2447342a3f0c838b270099aaf5fd571f0
MartinMendezSanti <martin.a.mendez91@gmail.com>
2020-11-02 21:25:01 -0300

    se completaron los funciones y se agregaron controles

--- parcial.py
+++ parcial.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 
 def CargarDatosModificar(archivo):
     try:
@@ -9,8 +10,15 @@ def CargarDatosModificar(archivo):
             while flag == "si":
                 lista_empleado = []
                 for x in range (4):
-                    dato = input(f"Ingrese el {campos[x]} del empleado")
-                    lista_empleado.append(dato)
+                    if x == 0 or x == 3:
+                        try:
+                            dato = int(input(f"Ingrese el {campos[x]} del empleado\n"))
+                            lista_empleado.append(dato)
+                        except ValueError:
+                            print(f"Se debe ingresar un número entero para el campo {campos[x]}")
+                    else:
+                        dato = input(f"Ingrese el {campos[x]} del empleado\n")
+                        lista_empleado.append(dato)
                 escritor.writerow(lista_empleado)
                 flag = input("Desea ingresar otro empleado? si/no\n")
     except IOError:
@@ -25,8 +33,15 @@ def CargarDatosNuevo(archivo):
             while flag == "si":
                 lista_empleado = []
                 for x in range (4):
-                    dato = input(f"Ingrese el {campos[x]} del empleado")
-                    lista_empleado.append(dato)
+                    if x == 0 or x == 3:
+                        try:
+                            dato = int(input(f"Ingrese el {campos[x]} del empleado\n"))
+                            lista_empleado.append(dato)
+                        except ValueError:
+                            print(f"Se debe ingresar un número entero para el campo {campos[x]}")
+                    else:
+                        dato = input(f"Ingrese el {campos[x]} del empleado\n")
+                        lista_empleado.append(dato)
                 escritor.writerow(lista_empleado)
                 flag = input("Desea ingresar otro empleado? si/no\n")
     except IOError:
@@ -34,16 +49,42 @@ def CargarDatosNuevo(archivo):
 
 
 
-def ConsultarDias():
-    print("Hola")
+def ConsultarDias(archivo, buscador):
+    try:
+        with open (archivo) as f_empleados, open("dias.csv") as f_dias:
+            empleados_csv = csv.reader(f_empleados)
+            dias_csv = csv.reader(f_dias)
+            next(dias_csv, None)
+
+            empleado = next(empleados_csv, None)
+            dias = next(dias_csv, None)
+
+            while empleado:
+                legajo, apellido, nombre, vacaciones = empleado
+                vacaciones = int(vacaciones)
+                tomados = 0
+                while dias and dias[0] == legajo:
+                    tomados += 1
+                    dias = next(dias_csv, None)
+                print(f"{legajo} : {buscador}")
+                if legajo == buscador:
+                    restante = vacaciones - tomados
+                    print(f"Legajo {legajo}: {nombre} {apellido}, le restan {restante} de vacaciones")
+                    break
+                empleado = next(empleados_csv, None)
+    except IOError:
+        print("Hubo un problema al intentar abrir el archivo")
 
 print("Bienvenido al gestor de vacaciones de empleados")
-lista_archivos = []
+lista_archivos = ["empleados.csv"] #lista donde se van a guardar los nombres de archivos.csv
+
+#se crea el menú con los controles de que no se ingresen caracteres erroneos.
 flag = "0"
 while flag != "3":
     flag = input("Ingrese el número de opción que desea realizar.\n1- Cargar Datos de Legajo de los empleados\n2- Consultar los días restantes de vacaciones de un empleado\n3- Salir\n")
     if flag == "1":
         nombre_archivo = input("Ingrese el nombre del archivo csv donde desea cargar los datos\n")
+        #se fija en en lista_archivos si ya existe ese archivo; si es así se pregunta si se lo quiere modificar o sobreescribir. Si no existe se usa la misma función que sobreescribir.
         if nombre_archivo in lista_archivos:
             opcion = "0"
             while opcion != "1" and opcion != "2":
@@ -59,9 +100,13 @@ while flag != "3":
             lista_archivos.append(nombre_archivo)
             CargarDatosNuevo(nombre_archivo)
 
-
     if flag == "2":
-        ConsultarDias()
+        legajo_buscado = input("Ingrese el número de legajo a buscar\n")
+        id_archivo = input("Ingrese el nombre del archivo de empleados que desea utilizar\n")
+        if id_archivo in lista_archivos:
+            ConsultarDias(id_archivo, legajo_buscado)
+        else:
+            print("No existe un archivo con ese nombre")
     if flag == "3":
         print("Gracias por utilizar el sistema")
     if flag != "1" and flag != "2" and flag != "3":
